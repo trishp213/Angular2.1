c5224fc1b1f44a4138c327dedf6865c02829d1d4
Trish Polay <trishp@imaginet.com>
2016-11-23 16:01:27 -0700

    Added first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    selector: 'pm-app',
+    template: `<div><h1>{{pageTitle}}</h1>
+                </div>`
 })
-export class AppComponent { }
+export class AppComponent { 
+    pageTitle: string = "Acme Product Management"
+}
