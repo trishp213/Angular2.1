88630a31bf144f25431115f83e593cd161289a4e
Trish Polay <trishp@imaginet.com>
2016-11-24 10:25:35 -0700

    Added jquery

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,7 @@ import { Component } from '@angular/core';
     selector: 'pm-app',
     template: `<div><h1>{{pageTitle}}</h1>
                 <pm-products></pm-products>
-                </div>`,
-    
+                </div>`    
 })
 export class AppComponent { 
     pageTitle: string = "Acme Product Management"
