4b7897db03fade5d46a32e0e1301fa5ca338245d
Trish Polay <trishp@imaginet.com>
2016-11-25 13:12:01 -0700

    product service

--- app/products/product.module.ts
+++ app/products/product.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductFilterPipe } from './product-filter.pipe';
-
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductFilterPipe
+  ],
+    providers: [
+    ProductService,
   ]
+
 })
 export class ProductModule {}
